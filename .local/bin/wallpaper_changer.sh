ff9068063aca1784226b9fe1386010237c208939
SV <suivue@tutamail.com>
2026-04-06 18:37:24 +0530

    Update scripts

--- .local/bin/wallpaper_changer.sh
+++ .local/bin/wallpaper_changer.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-DISPLAY=:0 feh --no-fehbg --bg-scale --randomize --recursive \
-    /usr/share/backgrounds/
+IMGDIR="/usr/share/backgrounds"
+DISPLAY=:0 feh --no-fehbg --bg-scale --randomize --recursive "$IMGDIR"
 
-# Note: Cron job (or systemd service/timer) does NOT work without 
-# DISPLAY=:0
+# Note: Cron job (or systemd service/timer) does NOT work without
+# `DISPLAY=:0`
 
 # A sample crontab entry: runs the script every 10m.
 # m h dom mon dow command
